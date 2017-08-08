23648d1480bbcda129d8e31f540b8fb54c089b1a
Progyan Bhattacharya <progyanb@acm.org>
2017-08-09 02:19:15 +0530

    Test: Changed Test Script
    Signed-off-by: Progyan Bhattacharya <progyanb@acm.org>

--- test/test.js
+++ test/test.js
@@ -16,18 +16,54 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-// Requiring test cases and their functions
-const HelloWorld = require('./HelloWorld');
+const Stimulator = require('../src'),
+      path       = require('path'),
+      fs         = require('fs'),
+      exit       = process.exit;
 
 // Defining states for test
-const mapStateToTest = {
-    "Basic I/O": HelloWorld
+let mapStateToTest = {
+    "Basic I/O": "HelloWorld"
 };
 
-// Test starts
-Object.keys(mapStateToTest).forEach((item, index) => {
-    console.log(`${index + 1}: Testing MIPS-Stimulator for functionality: ${item}\n`);
-    if (mapStateToTest.hasOwnProperty(item)) {
-        mapStateToTest[item]();
+// Testing function
+function  test(testCase) {
+    const infile     = path.join(__dirname, `./${testCase}/${testCase}.s`),
+          outfile    = path.join(__dirname, `./${testCase}/${testCase}.out`),
+          expfile    = path.join(__dirname, `./${testCase}/${testCase}.exp`),
+          testfile   = path.join(__dirname, `./${testCase}/${testCase}.test`);
+
+    if (! (fs.existsSync(infile) && fs.existsSync(expfile))) {
+        console.error(`Error: Input file specified for testcase cannot be found!`);
+        exit(1);
     }
+
+    const stdout     = fs.createWriteStream(`${testfile}`),
+          expected   = fs.readFileSync(`${expfile}`, 'ASCII'),
+          stimulator = new Stimulator({ infile, outfile, stdout });
+
+    stimulator.compile();
+
+    stdout.on('error', (err) => {
+        console.error(`Test for MIPS-Stimulator failed with error ${err}!\n`);
+        exit(1);
+    });
+
+    stdout.on('close', () => {
+        console.log("Finished execution. Collecting results...");
+        const output = fs.readFileSync(`${testfile}`, 'ASCII');
+
+        if (output === expected) {
+            console.log("Test for MIPS-Stimulator is successful!\n");
+        }  else {
+            console.error("Test for MIPS-Stimulator failed with wrong output!\n");
+            exit(1);
+        }
+    });
+}
+
+// Test starts
+Object.entries(mapStateToTest).forEach((item, index) => {
+    console.log(`${index + 1}: Testing MIPS-Stimulator for functionality: ${item[0]}\n`);
+    test(item[1]);
 });
