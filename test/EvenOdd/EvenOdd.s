84a0f67d603a33ade57194db4cb4bc6c25221c12
Progyan Bhattacharya <progyanb@acm.org>
2018-01-22 18:43:07 +0530

    Branch: Defined Implementations and Test Script
    Created method inside Virtual Machine module to make branch decision and update Program Counter accordingly. Created an assembly program to check if a given number is even or odd as testbench.
    
    Issue: Division does not work as expected, calls valueFromRegister twice for each entry.
    Signed-off-by: Progyan Bhattacharya <progyanb@acm.org>

---
+++ test/EvenOdd/EvenOdd.s
@@ -0,0 +1,38 @@
+# EvenOdd.s
+#
+# This program checks if a number is Even or Odd
+# Copyright (C) 2017 Bytes Club <bytes-club@googlegroups.com>
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+	.data	# Data declaration
+# Output Strings
+ev_string:	.asciiz	"\nThe Number is Even\n"
+od_string:	.asciiz	"\nThe Number is Odd\n"
+
+	.text	# Assembly Instructions
+main:		# Code starts here
+    li $s0, 52
+    li $s1, 2
+    div $s1, $s0
+    bez $hi, even           # Check if remainder is zero
+	li $v0, 4				# System call code for printing string is 4
+	la $a0, od_string			# Output string passed as argument
+	syscall					# This will call OS to print ($a0)
+	li $v0, 10				# System call code for exit is 10
+	syscall					# Program terminated by OS
+even:
+    li $v0, 4				# System call code for printing string is 4
+    la $a0, ev_string			# Output string passed as argument
+    syscall					# This will call OS to print ($a0)
+    li $v0, 10				# System call code for exit is 10
+    syscall					# Program terminated by OS
